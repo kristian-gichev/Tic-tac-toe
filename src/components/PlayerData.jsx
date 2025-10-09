bb22fdcf44c9e0edf1de9f824962dfe4396970b6
Kristian Gichev <kristian.gichev@xtrmnd.com>
2025-10-09 11:56:23 +0300

    Added player name editing feature.

--- src/components/PlayerData.jsx
+++ src/components/PlayerData.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 export default function PlayerData({ player }) {
+    const [name, setName] = useState(player.name);
+    const [isEditing, setIsEditing] = useState(false);
+
     return (
         <>
             <span className="player">
-                <span className="player-name">{player.name}</span>
+                <span className="player-name">{isEditing ? (
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                ) : name}</span>
                 <span className="player-symbol">{player.symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={() => setIsEditing(!isEditing)}>
+                {isEditing ? "Save" : "Edit"}
+            </button>
         </>
     );
 }
